56f8b11d4ccad92198aa085acc6352d417e8f4b7
Jane Yefimenko <jane.yefimemko@gmail.com>
2023-01-12 15:26:55 +0200

    Code refactoring
    deleted similar variables and updated storing logic

--- app.js
+++ app.js
@@ -1,63 +1,3 @@
-let advertItems;
-if (localStorage.getItem('adverData')) {
-  advertItems = JSON.parse(localStorage.getItem('adverData'));
-} else {
-  advertItems = [
-    {
-      imageSrc: './img/powerbank.jpg',
-      userName: 'Joseph',
-      userSurname: 'Doe',
-      userPhone: '0602332444',
-      userEmail: 'joseph@gmail.net',
-      title: 'Anker 737 Power Bank 140W 24000mAh',
-      price: '149.99',
-      description: `
-    Ultra-Powerful Two-Way Charging:</b> Equipped with the latest
-    Power Delivery 3.1 and bi-directional technology to quickly recharge
-    the portable charger or get a 140W ultra-powerful charge.
-    High Capacity and Long-Lasting: Featuring a 24,000mAh battery
-    capacity and 2x longer-lasting battery life, juice up an iPhone 13
-    almost 5 times or a 2021 iPad Pro 12.9" 1.3 times.
-    Smart Digital Display: Easy-to-read digital display shows the
-    output and input power and estimated time for the portable charger
-    to fully recharge.
-    What You Get: Anker 737 Power Bank (PowerCore 24K), welcome
-    guide, 140W USB-C to USB-C cable, 24-month stress-free warranty, and
-    friendly customer service.`,
-      term: 'December 2022',
-    },
-    {
-      imageSrc: './img/Charger_747.jpg',
-      userName: 'Maxim',
-      userSurname: 'Doe',
-      userPhone: '0602332444',
-      userEmail: 'maxim@gmail.net',
-      title: 'Some Charger 140W 24000mAh',
-      price: '1000.00',
-      description: `
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consectetur, 
-    tenetur aspernatur nulla ducimus rem laboriosam doloribus aperiam repudiandae autem 
-    nesciunt accusantium optio quibusdam eveniet? Quisquam doloremque eveniet earum dicta.
-    `,
-      term: 'January 2023',
-    },
-    {
-      imageSrc: './img/PowerStation_767.jpg',
-      userName: 'Veronika',
-      userSurname: 'Doe',
-      userPhone: '0802233444',
-      userEmail: 'ver@gmail.com',
-      title: 'Some PowerStation',
-      price: '10856.00',
-      description: `
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consectetur, 
-    tenetur aspernatur nulla ducimus rem laboriosam doloribus aperiam repudiandae autem 
-    nesciunt accusantium optio quibusdam eveniet? Quisquam doloremque eveniet earum dicta.
-    `,
-      term: 'December 2023',
-    },
-  ];
-}
 
 const advertItem = {};
 const advertForm = document.querySelector('#advert-form');
@@ -69,8 +9,8 @@ function errorHandling(obj) {
         throw `One or more fields are EMPTY`; // generates an exception
       }
     }
-    advertItems.push(obj);
-    localStorage.setItem('adverData', JSON.stringify(advertItems)); // add data to local storage
+    advertBase.values.push(obj);
+    localStorage.setItem('adverData', JSON.stringify(advertBase.values)); // add data to local storage
   } catch (e) {
     console.error(e); // pass exception object to error handler
   }
@@ -109,8 +49,8 @@ if (advertForm) {
 const advertContainer = document.querySelector('.cards-container');
 
 if (advertContainer) {
-  for (let index = 0; index < advertItems.length; index++) {
-    const element = advertItems[index];
+  for (let index = 0; index < advertBase.quantity(); index++) {
+    const element = advertBase.values[index];
 
     advertContainer.insertAdjacentHTML(
       'beforeend',
@@ -131,7 +71,7 @@ if (advertContainer) {
   }
 }
 
-/////////////////// My code ///////////////////////////////
+
 // Render single item page by clicking on main page item
 const cards = [...document.querySelectorAll('.card')];
 cards.forEach(card => {
@@ -146,7 +86,7 @@ cards.forEach(card => {
 const postPage = document.querySelector('#post');
 if (postPage) {
   // Image field
-  const singleImg = document.querySelector('img');
+  const singleImg = document.querySelector('.photo img');
 
   //Name field
   const userName = document.querySelector('.name');
@@ -172,7 +112,7 @@ if (postPage) {
     userName.innerText = advertBase.values[idx].userName;
     termDate.innerText = advertBase.values[idx].term;
     title.innerText = advertBase.values[idx].title;
-    price.innerText = advertBase.values[idx].price;
+    price.innerText = `$ ${advertBase.values[idx].price}`;
     description.innerText = advertBase.values[idx].description;
   };
 
